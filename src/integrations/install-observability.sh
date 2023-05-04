04a9900082a0c86b190ae9b017cede7aa86eeeed
YANGDB <yang.db.dev@gmail.com>
2023-05-04 13:53:45 -0700

    add monitoring & opinionated assets for this demo
    Signed-off-by: YANGDB <yang.db.dev@gmail.com>

--- src/integrations/install-observability.sh
+++ src/integrations/install-observability.sh
@@ -12,5 +12,27 @@ until nc -z -v -w30 "$host" "$port"; do
 done
 
 echo "Service is up and ready! Executing command:"
-echo "importing assets into opensearch"
+# create datasource for prometheus
+echo "create datasource for prometheus"
 exec $cmd
+
+# create index template for logs,traces,metrics
+echo "create index template for logs,traces,metrics,serviceMaps"
+exec $cmd
+
+# create data-streams for logs,traces,metrics
+echo "create data-streams for logs,traces,metrics"
+exec $cmd
+
+# create email channel for alerting
+echo " create email channel for alerting"
+exec $cmd
+
+# create saves searches for services KPI
+echo " create saves searches for services KPI"
+exec $cmd
+
+# create Alerts queries for monitoring services
+echo " create Alerts queries for monitoring services
+exec $cmd
+
