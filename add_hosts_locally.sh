0148bb3d43c077ac3a2169492bafe3ba19cb227f
YANGDB <yang.db.dev@gmail.com>
2023-05-15 11:01:12 -0700

    adding integrationbs docker-compose service for assets loading into opensearch / dashboards
    Signed-off-by: YANGDB <yang.db.dev@gmail.com>

--- add_hosts_locally.sh
+++ add_hosts_locally.sh
@@ -1,6 +1,3 @@
-# Copyright The OpenTelemetry Authors
-# SPDX-License-Identifier: Apache-2.0
-
 #!/bin/bash
 # Local host that are used to help developing and debugging the OTEL demo locally
 
@@ -11,12 +8,8 @@ IP="127.0.0.1"
 OPENSEARCH_HOST="opensearch-node1"
 OPENSEARCH_DASHBOARD="opensearch-dashboards"
 OTEL_STORE="frontend"
-FEATURE_FLAG_SERVICE="feature-flag-service"
-NGINX="nginx"
 OTEL_LOADER="loadgenerator"
 PROMETHEUS="prometheus"
-LOAD_GENERATOR="load-generator"
-GRAFANA="grafana"
 
 # Add the entry to the /etc/hosts file
 echo "$IP    $OPENSEARCH_HOST" | sudo tee -a /etc/hosts
@@ -24,7 +17,3 @@ echo "$IP    $OPENSEARCH_DASHBOARD" | sudo tee -a /etc/hosts
 echo "$IP    $OTEL_STORE" | sudo tee -a /etc/hosts
 echo "$IP    $PROMETHEUS" | sudo tee -a /etc/hosts
 echo "$IP    $OTEL_LOADER" | sudo tee -a /etc/hosts
-echo "$IP    $NGINX" | sudo tee -a /etc/hosts
-echo "$IP    $LOAD_GENERATOR" | sudo tee -a /etc/hosts
-echo "$IP    $FEATURE_FLAG_SERVICE" | sudo tee -a /etc/hosts
-echo "$IP    $GRAFANA" | sudo tee -a /etc/hosts
